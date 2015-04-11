6b82cdf9b741eb1f542881670f5265e446f5b7c4
John McCaffrey <john@theartboy.net>
2015-04-11 09:16:32 -0500

    Move show hides from viewDidLoad to viewDidAppear. Add microphone button enable/disable

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var microphoneButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        recordingLabel.hidden = true
+    }
+    override func viewWillAppear(animated: Bool) {
+        recordingLabel.hidden = true
+        stopButton.hidden = true
+        microphoneButton.enabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,11 +31,14 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(sender: UIButton) {
 //        TODO: record audio
         recordingLabel.hidden = false
+        stopButton.hidden = false
+        microphoneButton.enabled = false
         
     }
 
     @IBAction func stopRecording(sender: UIButton) {
         recordingLabel.hidden = true
+        stopButton.hidden = true
     }
 }
 
