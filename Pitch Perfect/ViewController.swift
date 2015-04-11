f12825e310057e19593d1b2b6d7cdae145a704a2
John McCaffrey <john@theartboy.net>
2015-04-10 22:52:27 -0500

    Add recording label that toggles with each touch event

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var recordingLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        recordingLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+//        TODO: show text recording in progress
+//        TODO: record audio
+        
+//        println("recording audio")
+        recordingLabel.hidden = !recordingLabel.hidden
+        
+    }
 
 }
 
