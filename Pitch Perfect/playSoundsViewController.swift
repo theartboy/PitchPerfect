9bc3238d5808bebe5bc9f3839b0fa1463f0507cc
John McCaffrey <john@theartboy.net>
2015-04-16 11:09:59 -0500

    Add playSoundViewController. Add slow button with stub function

---
+++ Pitch Perfect/playSoundsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  playSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by John McCaffrey on 4/16/15.
+//  Copyright (c) 2015 John McCaffrey. All rights reserved.
+//
+
+import UIKit
+
+class playSoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PlaySoundsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by John McCaffrey on 4/16/15.
+//  Copyright (c) 2015 John McCaffrey. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    @IBOutlet weak var slowButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
