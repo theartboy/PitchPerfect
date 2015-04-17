1d3ea5d634db3e07d1002c5821a83025f5d02f1b
John McCaffrey <john@theartboy.net>
2015-04-16 21:57:27 -0500

    Clean up play mp3 code. Add play slow code. Add fast and stop buttons

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -12,36 +12,49 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
 
     @IBOutlet weak var slowButton: UIButton!
-    var audioPlayer: AVAudioPlayer?
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let url = NSURL.fileURLWithPath(
-            NSBundle.mainBundle().pathForResource("movie_quote",
-                ofType: "mp3")!)
+//        let url = NSURL.fileURLWithPath(
+//            NSBundle.mainBundle().pathForResource("movie_quote",
+//                ofType: "mp3")!)
+//        
+//        var error: NSError?
+//        
+//        audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
+//        
+//        if let err = error {
+//            println("audioPlayer error \(err.localizedDescription)")
+//        } else {
+////            audioPlayer?.delegate = self
+//            audioPlayer?.prepareToPlay()
+//        }
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
+            audioPlayer.prepareToPlay()
+            
+        }else{
+            println("file path is empty or incorrect")
+        }
+    }
         
-        var error: NSError?
-        
-        audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
-        
-        if let err = error {
-            println("audioPlayer error \(err.localizedDescription)")
-        } else {
-//            audioPlayer?.delegate = self
-            audioPlayer?.prepareToPlay()
-        }    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func playSlowAudio(sender: UIButton) {
-        if let player = audioPlayer {
-            player.play()
-        }
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
+//        if let player = audioPlayer {
+//            player.play()
+//        }
     }
 
     /*
